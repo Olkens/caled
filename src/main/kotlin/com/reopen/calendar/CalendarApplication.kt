6e0b8a3f13f16c89b065e1fb3ec7a8e5ededa676
olkusz <olkusz.michal@gmail.com>
2026-04-23 16:52:47 +0200

    cors and timeslot entity

--- src/main/kotlin/com/reopen/calendar/CalendarApplication.kt
+++ src/main/kotlin/com/reopen/calendar/CalendarApplication.kt
@@ -2,7 +2,9 @@ package com.reopen.calendar
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
+@EnableJpaAuditing
 @SpringBootApplication
 class CalendarApplication
 
---
+++ src/main/kotlin/com/reopen/calendar/domain/TimeSlot.kt
@@ -0,0 +1,35 @@
+package com.reopen.calendar.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "time_slot")
+@EntityListeners(AuditingEntityListener::class)
+class TimeSlot(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0,
+
+    @Column(name = "time_start", nullable = false)
+    var timeStart: LocalDateTime,
+
+    @Column(name = "time_stop", nullable = false)
+    var timeStop: LocalDateTime,
+
+    @CreatedDate
+    @Column(name = "date_created", nullable = false, updatable = false)
+    var dateCreated: LocalDateTime = LocalDateTime.now(),
+
+    @LastModifiedDate
+    @Column(name = "date_updated", nullable = false)
+    var dateUpdated: LocalDateTime = LocalDateTime.now()
+)

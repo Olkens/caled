e5da9af012dd1873b3ffffc99e53062f8388f0bc
olkusz <olkusz.michal@gmail.com>
2026-04-27 20:58:26 +0200

    timeslot get endpoint to get timeslots in between dates

--- src/main/kotlin/com/reopen/calendar/controllers/TimeSlotRESTController.kt
+++ src/main/kotlin/com/reopen/calendar/controllers/TimeSlotRESTController.kt
@@ -1,18 +1,34 @@
 package com.reopen.calendar.controllers
 
+import com.reopen.calendar.domain.TimeSlot
 import com.reopen.calendar.dto.timeslot.TimeSlotDTO
+import com.reopen.calendar.repositories.TimeSlotRepository
 import com.reopen.calendar.services.TimeSlotService
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
 
 @RestController
 @RequestMapping("/api/timeslots")
 class TimeSlotRESTController (
-    private val timeSlotService: TimeSlotService
+    private val timeSlotService: TimeSlotService,
+    private val repository: TimeSlotRepository
 ) {
+
+    @GetMapping
+    fun getTimeSlots(
+        @RequestParam start: String,
+        @RequestParam end: String
+    ): List<TimeSlot> {
+        val from = LocalDateTime.parse(start)
+        val to = LocalDateTime.parse(end)
+        return repository.findAllByTimeStartBetween(from, to)
+    }
 
     @PostMapping("/")
     fun saveTimeSlot(
--- src/main/kotlin/com/reopen/calendar/repositories/TimeSlotRepository.kt
+++ src/main/kotlin/com/reopen/calendar/repositories/TimeSlotRepository.kt
@@ -2,6 +2,8 @@ package com.reopen.calendar.repositories
 
 import com.reopen.calendar.domain.TimeSlot
 import org.springframework.data.jpa.repository.JpaRepository
+import java.time.LocalDateTime
 
 interface TimeSlotRepository : JpaRepository<TimeSlot, Long> {
+    fun findAllByTimeStartBetween(start: LocalDateTime, end: LocalDateTime): List<TimeSlot>
 }
